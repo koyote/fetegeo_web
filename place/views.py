b3fdd90a904f39a1c59bde4b8222a73d4cff231f
Pit <apps.pit@gmail.com>
2013-02-08 21:50:45 +0000

    Add error handling for empty results

--- place/views.py
+++ place/views.py
@@ -1,10 +1,5 @@
 from django.shortcuts import render_to_response
 from place.models import PlaceName
-from django.template.loader import render_to_string
-
-
-TITLE = 'Fetegeo Mapping'
-
 
 def index(request):
     error = False
@@ -15,5 +10,8 @@ def index(request):
             error = True
         else:
             results = PlaceName.objects.filter(name__iexact=query)
-            return render_to_response('index.html', {'results': results})
+            if results.count() == 0 or all(r.place.location is None for r in results):
+                return render_to_response('index.html', {'no_result': True, 'q': query})
+            else:
+                return render_to_response('index.html', {'results': results})
     return render_to_response('index.html', {'error': error})
