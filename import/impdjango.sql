43cdce904f0559073b33dbca9b737a5f1a1a89b9
Pit <apps.pit@gmail.com>
2013-02-15 21:06:44 +0000

    Add osmosis to import script

--- import/impdjango.sql
+++ import/impdjango.sql
@@ -1,38 +1,50 @@
+-- Clustering place location
 CLUSTER place_location_id ON place;
+
+-- Clustering postcode location
 CLUSTER postcode_location_id ON postcode;
 
+-- Building postcode areas
 UPDATE postcode
 SET location = ST_BuildArea(location)
 WHERE ST_BuildArea(location) IS NOT NULL;
 
+-- Building place areas
 UPDATE place
 SET location = ST_BuildArea(location)
 WHERE ST_BuildArea(location) IS NOT NULL;
 
+-- Building homogenising place locations
 UPDATE place
 SET location = ST_CollectionHomogenize(location);
 
+-- Building homogenising postcode locations
 UPDATE postcode
 SET location = ST_CollectionHomogenize(location);
 
+-- Removing invalid place locations
 UPDATE place
 SET location = null
 WHERE NOT ST_IsValid(location);
 
+-- Removing invalid postcode locations
 UPDATE postcode
 SET location = null
 WHERE NOT ST_IsValid(location);
 
+-- Updating postcode countries
 UPDATE postcode
 SET country_id = place.country_id
 FROM place
 WHERE place.country_id IS NOT NULL AND ST_Covers(place.location, postcode.location);
 
+-- Updating place countries
 UPDATE place
 SET country_id = p2.country_id
 FROM place as p2
 WHERE p2.country_id IS NOT NULL AND ST_Covers(p2.location, place.location);
 
+-- Updating place parents
 UPDATE place
 SET parent_id = b_id
 FROM (
@@ -48,6 +60,7 @@ FROM (
 ) pp
 WHERE id = s_id;
 
+-- Updating postcode parents
 UPDATE postcode
 SET parent_id = b_id
 FROM (
--- importer.py
+++ importer.py
@@ -3,11 +3,16 @@ from django.core.management import call_command
 import fetegeo_web.settings as settings
 import os
 import time
+import subprocess
+import shlex
+import getopt
+import sys
 management.setup_environ(settings)
 from django.db import connection
 
 
-tables = ['type', 'country', 'lang', 'place', 'postcode', 'place_name']
+_TABLES = ['type', 'country', 'lang', 'place', 'postcode', 'place_name']
+_IMPORT_DIR = os.path.join(os.path.dirname(__file__), 'import/').replace('\\', '/')
 
 class Timer:    
     def __enter__(self):
@@ -17,12 +22,13 @@ class Timer:
     def __exit__(self, *args):
         self.end = time.time()
         self.interval = self.end - self.start
+        
 
 def open_file(txt):
-    return open(os.path.join(os.path.dirname(__file__), 'import/' + txt).replace('\\', '/'))
+    return open(_IMPORT_DIR+txt)
 
 def import_data(cursor):
-    for table in tables:
+    for table in _TABLES:
         with open_file(table + '.txt') as f:
             print("Importing data for: " + table)
             cursor.copy_from(f, table)
@@ -32,29 +38,53 @@ def execute_postgis(cursor):
     with open_file('impdjango.sql') as f:
         print("Executing PostGIS statments...")
         query = ''
+        comment = ''
         for line in f:
+            if line.startswith("--"):
+                comment = line.replace("--", "")
+                continue
+            
             query += line
+            
             if ";" in line:
-                print("Executing: " + query) 
+                print("Executing: " + comment.strip()) 
                 with Timer() as t:
                     cursor.execute(query)
-                print("Time: %.03f seconds" % t.interval)
-                query = ''
+                print("Time: %.03f seconds.\n" % t.interval)
+                query = comment = ''
 
 def vacuum_analyze(cursor):
     old_iso_level = connection.connection.isolation_level
     connection.connection.set_isolation_level(0)
-    print('Executing VACUUM ANALYZE')
+    print('Executing: VACUUM ANALYZE')
     cursor.execute('VACUUM ANALYZE')
     connection.connection.set_isolation_level(old_iso_level)
     
+def usage(error):
+    print(error)
+    
 if __name__ == "__main__":
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], 'f:')
+    except getopt.error as e:
+        usage(str(e))
+    osm_file = ''    
+    for opt, arg in opts:
+        if opt == '-f':
+            osm_file = arg
+    
+    if not osm_file:
+        usage("No file")
+    else:
+        osmosis_command = shlex.split('osmosis --fast-read-xml file="' + osm_file + '" --fimp outdir=' + _IMPORT_DIR)
+        subprocess.check_output(osmosis_command)
+
     with Timer() as t:
-        call_command('syncdb', interactive=False)  # Create DB tables if they don't exist already.
+        call_command('syncdb', interactive=False)  # Create DB _TABLES if they don't exist already.
         call_command('flush', interactive=False)  # Delete all data if there was data.
         cursor = connection.cursor()
         
         import_data(cursor)
         execute_postgis(cursor)
         vacuum_analyze(cursor)
-    print("Total Time: %.03f minutes" % t.interval/60.0)
+    print("Total Time: %.03f seconds." % t.interval)
