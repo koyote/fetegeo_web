f0b3f5fbe52abe045c9237b78a340f29f8b3bc50
Pit <apps.pit@gmail.com>
2013-02-15 21:50:24 +0000

    Improve timer output

--- importer.py
+++ importer.py
@@ -17,25 +17,28 @@ _IMPORT_DIR = os.path.join(os.path.dirname(__file__), 'import/').replace('\\', '
 class Timer:    
     def __enter__(self):
         self.start = time.time()
-        return self
 
     def __exit__(self, *args):
-        self.end = time.time()
-        self.interval = self.end - self.start
-        
+        interval = time.time() - self.start
+        if interval < 120:
+            print("Time: {:.2f} seconds.".format(interval))
+        elif interval < 7200:
+            m, s = divmod(interval, 60)
+            print("Time: {0:.0f} minutes, {1:.0f} seconds.".format(m, s))
+        else:
+            m, s = divmod(interval, 60)
+            h, m = divmod(m, 60)
+            print("Time: {0:.0f} hours, {1:.0f} minutes and {0:.0f} seconds.".format(h, m, s))
 
-def open_file(txt):
-    return open(_IMPORT_DIR+txt)
-
-def import_data(cursor):
+def _import_data(cursor):
     for table in _TABLES:
-        with open_file(table + '.txt') as f:
+        with open(_IMPORT_DIR + table + '.txt') as f:
             print("Importing data for: " + table)
             cursor.copy_from(f, table)
             connection.commit()
             
-def execute_postgis(cursor):
-    with open_file('impdjango.sql') as f:
+def _execute_postgis(cursor):
+    with open(_IMPORT_DIR + 'impdjango.sql') as f:
         print("Executing PostGIS statments...")
         query = ''
         comment = ''
@@ -48,43 +51,39 @@ def execute_postgis(cursor):
             
             if ";" in line:
                 print("Executing: " + comment.strip()) 
-                with Timer() as t:
+                with Timer():
                     cursor.execute(query)
-                print("Time: %.03f seconds.\n" % t.interval)
                 query = comment = ''
 
-def vacuum_analyze(cursor):
+def _vacuum_analyze(cursor):
     old_iso_level = connection.connection.isolation_level
     connection.connection.set_isolation_level(0)
     print('Executing: VACUUM ANALYZE')
     cursor.execute('VACUUM ANALYZE')
     connection.connection.set_isolation_level(old_iso_level)
     
-def usage(error):
-    print(error)
-    
 if __name__ == "__main__":
     try:
         opts, args = getopt.getopt(sys.argv[1:], 'f:')
     except getopt.error as e:
-        usage(str(e))
+        print(str(e))
+        
     osm_file = ''    
     for opt, arg in opts:
         if opt == '-f':
             osm_file = arg
     
     if not osm_file:
-        usage("No file")
+        print("No OSM file mentioned. Add one with the -f command.")
     else:
         osmosis_command = shlex.split('osmosis --fast-read-xml file="' + osm_file + '" --fimp outdir=' + _IMPORT_DIR)
         subprocess.check_output(osmosis_command)
 
-    with Timer() as t:
+    with Timer():
         call_command('syncdb', interactive=False)  # Create DB _TABLES if they don't exist already.
         call_command('flush', interactive=False)  # Delete all data if there was data.
         cursor = connection.cursor()
         
-        import_data(cursor)
-        execute_postgis(cursor)
-        vacuum_analyze(cursor)
-    print("Total Time: %.03f seconds." % t.interval)
+        _import_data(cursor)
+        _execute_postgis(cursor)
+        _vacuum_analyze(cursor)
